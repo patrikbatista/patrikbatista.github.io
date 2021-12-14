05027ef1f0486004644c006b5c23258d0a7fd6b8
patrik <pcr_batista@hotmail.com>
2021-12-14 14:56:39 -0300

    adiciona arquivos app

---
+++ statefull/precache-manifest.036c0e39f70bf452eb1b8f46236f681d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5f9e726b03835866498068a10fa9a6e3",
+    "url": "/statefull/index.html"
+  },
+  {
+    "revision": "ef0d4a0ae1fbcb9baa0c",
+    "url": "/statefull/static/css/main.5d58c8a2.chunk.css"
+  },
+  {
+    "revision": "cbb6d5c7ef3b1ab528f2",
+    "url": "/statefull/static/js/2.9de1bedc.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/statefull/static/js/2.9de1bedc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ef0d4a0ae1fbcb9baa0c",
+    "url": "/statefull/static/js/main.4f2d71b0.chunk.js"
+  },
+  {
+    "revision": "09fe7c714a0c6f00447d",
+    "url": "/statefull/static/js/runtime-main.12f0595c.js"
+  }
+]);
