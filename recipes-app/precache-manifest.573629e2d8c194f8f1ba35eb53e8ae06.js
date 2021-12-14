8684de88021ad1cb61484635dcac36a30cbd3758
patrik <pcr_batista@hotmail.com>
2021-12-14 14:44:48 -0300

    adiciona arquivos app receitas

---
+++ recipes-app/precache-manifest.573629e2d8c194f8f1ba35eb53e8ae06.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cf1bdeddb445893301c9374d709a1e17",
+    "url": "/recipes-app/index.html"
+  },
+  {
+    "revision": "38ed6209876a4298f832",
+    "url": "/recipes-app/static/css/2.95258f03.chunk.css"
+  },
+  {
+    "revision": "c891c0ac41aeda2784d1",
+    "url": "/recipes-app/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "38ed6209876a4298f832",
+    "url": "/recipes-app/static/js/2.c0f18d12.chunk.js"
+  },
+  {
+    "revision": "61d738e70260ad8be2e6305d4190eeff",
+    "url": "/recipes-app/static/js/2.c0f18d12.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c891c0ac41aeda2784d1",
+    "url": "/recipes-app/static/js/main.7acf7374.chunk.js"
+  },
+  {
+    "revision": "25993f9b630f63b44851",
+    "url": "/recipes-app/static/js/runtime-main.1228032d.js"
+  },
+  {
+    "revision": "b89133460c9aef111d6e773396b5917d",
+    "url": "/recipes-app/static/media/blackHeartIcon.b8913346.svg"
+  },
+  {
+    "revision": "f87c47c37c0c0530fe7307af260caa25",
+    "url": "/recipes-app/static/media/drinkIcon.f87c47c3.svg"
+  },
+  {
+    "revision": "157945fffa619b919f7bb65fe11b7b43",
+    "url": "/recipes-app/static/media/exploreIcon.157945ff.svg"
+  },
+  {
+    "revision": "b27ba19ea033355b73926443af1756e9",
+    "url": "/recipes-app/static/media/mealIcon.b27ba19e.svg"
+  },
+  {
+    "revision": "74e487e504d5cf16d7832bef2fbacfea",
+    "url": "/recipes-app/static/media/profileIcon.74e487e5.svg"
+  },
+  {
+    "revision": "db81213596cb3beb56440bb39aa70c8c",
+    "url": "/recipes-app/static/media/searchIcon.db812135.svg"
+  },
+  {
+    "revision": "06215ed4cae23455c02abc804445ba74",
+    "url": "/recipes-app/static/media/shareIcon.06215ed4.svg"
+  },
+  {
+    "revision": "ea3b6ba893ba3908dd84f6170b75545e",
+    "url": "/recipes-app/static/media/whiteHeartIcon.ea3b6ba8.svg"
+  }
+]);
